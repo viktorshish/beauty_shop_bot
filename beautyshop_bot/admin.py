f19de2a25acbfdf98db60afe4bd2ebd7bdb06156
kiril <kirill2689@gmail.com>
2024-01-25 13:51:22 +0300

    Added first version of models, with Russian names

--- beautyshop_bot/admin.py
+++ beautyshop_bot/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from beautyshop_bot.models import Salon, TimeSlot, Speciality, Master, Client, Order
 
 # Register your models here.
+
+admin.site.register(Salon)
+admin.site.register(TimeSlot)
+admin.site.register(Master)
+admin.site.register(Order)
+admin.site.register(Speciality)
+admin.site.register(Client)
--- beautyshop_bot/apps.py
+++ beautyshop_bot/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class BeautyshopBotConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'beautyshop_bot'
+    verbose_name = "Информация о салонах"
