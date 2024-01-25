22f0e92865af3f14adf71247b5512d644cd7927a
Viktor Shishkalov Think <viktorshish@gmail.com>
2024-01-25 15:41:47 +0300

    Add keyboard

--- beautyshop_bot/management/commands/bot.py
+++ beautyshop_bot/management/commands/bot.py
@@ -3,6 +3,7 @@ import logging
 from django.core.management.base import BaseCommand
 from django.conf import settings
 from telegram.ext import Updater, CommandHandler
+from telegram import ReplyKeyboardMarkup
 
 
 logging.basicConfig(level=logging.INFO)
@@ -10,7 +11,15 @@ logger = logging.getLogger(__name__)
 
 
 def greet_user(update, context):
-    update.message.reply_text('Приветствую')
+    chat_id = update.effective_chat.id
+    with open('static/greting_salon.jpg', 'rb') as photo_file:
+        context.bot.send_photo(chat_id=chat_id, photo=photo_file)
+
+    start_keyboard = ReplyKeyboardMarkup([
+        ['Запись', 'Мои записи'],
+        ['Контакты салонов', 'Запись по телефону']
+    ])
+    update.message.reply_text('Приветствую', reply_markup=start_keyboard)
 
 
 class Command(BaseCommand):
